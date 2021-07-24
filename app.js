da8ffabe98183059f87590821374be808b9a9d6e
whatkimkong <whatkimkong@gmail.com>
2021-07-24 17:00:28 +0200

    Iteration 2, check  button links working, image not loading

--- app.js
+++ app.js
@@ -14,12 +14,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+
 // ...
 
 // Add the route handlers here:
 
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+
+app.get('/beer', (req, res) => {
+  res.render('beer');
+});
+
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
