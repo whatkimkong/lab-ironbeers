7dc4a97492b3532e428c95a1e3bc968cfa101b16
whatkimkong <whatkimkong@gmail.com>
2021-07-24 17:18:19 +0200

    Iteration 3.2, all preceding done, from 3.2 {{#each}} still pending.

--- app.js
+++ app.js
@@ -25,16 +25,24 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/beer', (req, res) => {
-  res.render('beer');
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database:', beersFromApi)
+    res.render('beer', beersFromApi);
+    })
+  .catch(error => console.log(error));
 });
 
 
 app.get('/random-beer', (req, res) => {
   punkAPI
-    .getBeers()
-    .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
-    .catch(error => console.log(error));
+  .getRandom()
+  .then(responseFromAPI => {
+    // your magic happens here
+  })
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
